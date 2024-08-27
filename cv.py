320f62c1d9316f5f2530479f9ff058f0087fe3c9
andrestaddei <0232911@up.edu.mx>
2024-08-27 09:11:05 -0600

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+st.markdown("# Currículum Vitae")
+st.markdown("## Andres Manuel Taddei Hurtado")
+
+st.markdown("""
+*Correo:* 0232911@up.edu.mx  
+*Teléfono:* +66 41274655  
+*Ubicación:* Guadalajara, Jalisco, México
+""")
+
+
+st.markdown("## Experiencia Laboral")
+st.markdown("""
+1. *Asesor Financiero Practicante* en Zazueta y Asociados ( Febrero 2024- Mayo 2024)  
+  
+
+""")
+
+st.markdown("## Educación")
+st.markdown("""
+- *Administración y Finanzas* en la Universidad Panamericana (2020 - Presente)
+- *Preparatoria* en Instituto Irlandes Hermosillo (Generación 2020)
+""")
+
+st.markdown("## Habilidades")
+st.markdown("""
+- Excel avanzado
+- Gestión de proyectos
+- Comunicación oral y escrita
+- Capacidad de liderazgo
+- Analisis de datos
+- Trabajo en equipo
+""")
+
+st.markdown("## Idiomas")
+st.markdown("""
+- *Español* - Nativo
+- *Inglés* - Avanzado
+- *Frances*- Básico
+""")
+
+st.markdown("## Intereses y logros adicionales")
+st.markdown("""
+- *Estudiante de intercambio en Francia* Claude Bernanrd Lyon I (Febrero a Junio 2023)
+- *Alumnos destacados Administracion y Finanzas*
+- *Certificación SAP* (Universidad Panamericana)
+""")
